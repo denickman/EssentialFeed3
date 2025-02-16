611a76fd4d7bd6144c79bac8d5627ee7b1cb1d89
Dennis Yaremenko <yaremdennis@gmail.com>
2025-02-16 09:26:23 +0200

    [Composition]: Move DEBUG code paths from the main SceneDelegate to a new DebugingSceneDelegate subclass to separate debug- and test- specific code from production code

--- EssentialFeed/EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialFeed/EssentialApp/EssentialApp/SceneDelegate.swift
@@ -23,12 +23,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
         
-#if DEBUG
-        if CommandLine.arguments.contains("-reset") {
-            try? FileManager.default.removeItem(at: localStoreURL)
-        }
-#endif
-        
         let remoteClient = makeRemoteClient()
         let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: remoteClient)
         let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
@@ -50,43 +44,24 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                     cache: localImageLoader)))
     }
     
+    /*
+     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+     guard let _ = (scene as? UIWindowScene) else { return }
+     
+     let url = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
+     
+     let session = URLSession(configuration: .ephemeral)
+     let client = URLSessionHTTPClient(session: session)
+     
+     let feedLoader = RemoteFeedLoader(url: url, client: client)
+     let imageLoader = RemoteFeedImageDataLoader(client: client)
+     
+     let feedViewController = FeedUIComposer.feedComposedWith(feedLoader: feedLoader, imageLoader: imageLoader)
+     
+     window?.rootViewController = feedViewController
+     */
+    
     func makeRemoteClient() -> HTTPClient {
-    #if DEBUG
-        if UserDefaults.standard.string(forKey: "connectivity") == "offline" {
-            return AlwaysFailingHTTPClient()
-        }
-    #endif
-        return URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+        URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
     }
 }
-
-/*
- func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
- guard let _ = (scene as? UIWindowScene) else { return }
- 
- let url = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
- 
- let session = URLSession(configuration: .ephemeral)
- let client = URLSessionHTTPClient(session: session)
- 
- let feedLoader = RemoteFeedLoader(url: url, client: client)
- let imageLoader = RemoteFeedImageDataLoader(client: client)
- 
- let feedViewController = FeedUIComposer.feedComposedWith(feedLoader: feedLoader, imageLoader: imageLoader)
- 
- window?.rootViewController = feedViewController
- */
-
-#if DEBUG
-private class AlwaysFailingHTTPClient: HTTPClient {
-    
-    private class Task: HTTPClientTask {
-        func cancel() {}
-    }
-    
-    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
-        completion(.failure(NSError.init(domain: "offline", code: 0)))
-        return Task()
-    }
-}
-#endif
