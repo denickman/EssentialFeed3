3d077ad59d9254614ed6cdc883740f7837eb65d5
Dennis Yaremenko <yaremdennis@gmail.com>
2025-03-31 13:56:03 +0300

    [007]: Deprecate async API in favor of new sync APIs

--- EssentialFeed/EssentialFeed/Feed Cache/FeedImageDataStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedImageDataStore.swift
@@ -13,6 +13,60 @@ public protocol FeedImageDataStore {
     typealias RetrievalResult = Swift.Result<Data?, Error>
     typealias InsertionResult = Swift.Result<Void, Error>
 
+    // async api
+    @available(*, deprecated)
     func insert(_ data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void)
+    
+    @available(*, deprecated)
     func retrieve(dataForURL url: URL, completion: @escaping (RetrievalResult) -> Void)
+    
+    // sync api
+    func insert(_ data: Data, for url: URL) throws
+    func retrieve(dataForURL url: URL) throws -> Data?
+}
+
+
+// temporary extension to not break clients since moving from async to sync api
+
+public extension FeedImageDataStore {
+    
+    func insert(_ data: Data, for url: URL) throws {
+        // возвращается Void (ничего), поэтому return try result.get() просто завершает выполнение или выбрасывает ошибку
+        let group = DispatchGroup()
+        
+        group.enter()
+        var result: InsertionResult!
+        
+        insert(data, for: url) {
+            result = $0
+            group.leave()
+        }
+        
+        group.wait()
+        return try result.get() // чтобы обработать возможную ошибку и завершить выполнение, так как метод ничего не возвращает
+    }
+    
+    func retrieve(dataForURL url: URL) throws -> Data? {
+        let group = DispatchGroup()
+        group.enter()
+        
+        var result: RetrievalResult!
+    
+        retrieve(dataForURL: url, completion: {
+            result = $0 // Result<Data?, Error>
+            group.leave()
+        })
+        
+        group.wait()
+        return try result.get()
+    }
+ 
+
+    func insert(_ data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void) {
+        
+    }
+    
+    func retrieve(dataForURL url: URL, completion: @escaping (RetrievalResult) -> Void) {
+        
+    }
 }
